9646038d3548c16aec9f1351d1fe630383a8bd85
emicsto <emicsto@gmail.com>
2020-01-25 23:22:39 +0100

    Add interceptor that adds access token to each request as Authorization header

--- lib/repositories/auth_repository.dart
+++ lib/repositories/auth_repository.dart
@@ -1,21 +1,19 @@
 import 'dart:convert';
 import 'package:flashcards/models/token_pair.dart';
-import 'package:http/http.dart' as http;
-import 'package:flashcards/utils/constants.dart' as Constants;
+import 'package:flashcards/utils/HttpDio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 Future<void> sendIdToken(String idToken) async {
-  print(Constants.API_URL + '/auth/token/sign-in');
-  final response = await http.post(
-      Constants.API_URL + '/auth/token/sign-in',
-      headers: {"Content-Type": "application/json"},
-      body: json.encode({"token": idToken})
+  var _dio = await getHttpClient();
+
+  final response = await _dio.post("/auth/token/sign-in",
+      data: json.encode({"token": idToken})
   );
 
   if (response.statusCode == 200) {
     final storage = new FlutterSecureStorage();
 
-    Map tokenPairMap = jsonDecode(response.body);
+    Map tokenPairMap = jsonDecode(response.data);
     var tokenPair = TokenPair.fromJson(tokenPairMap);
 
     await storage.write(key: "accessToken", value: tokenPair.accessToken);
